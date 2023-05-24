e32b639d66782983594edf3cc7242d73f59e60b1
Finn Bear <finnbearlabs@gmail.com>
2023-05-23 21:33:02 -0700

    Test 3.

--- packages/stylist-core/src/lib.rs
+++ packages/stylist-core/src/lib.rs
@@ -1,8 +1,2 @@
 mod error;
-pub use error::{Error, Result, ResultDisplay};
-pub mod ast;
-pub mod bow;
-
-#[cfg_attr(documenting, doc(cfg(feature = "parser")))]
-#[cfg(feature = "parser")]
-mod parser;
+pub use error::Error;
--- packages/stylist-macros/src/lib.rs
+++ packages/stylist-macros/src/lib.rs
@@ -1,53 +1,10 @@
 
 use proc_macro::TokenStream;
-use proc_macro_error::proc_macro_error;
 
-mod literal;
-
-
-#[proc_macro]
-#[proc_macro_error]
-pub fn sheet(input: TokenStream) -> TokenStream {
-    //sheet::macro_fn(input.into()).into()
-    Default::default()
-}
-
-#[proc_macro]
-#[proc_macro_error]
-pub fn style(input: TokenStream) -> TokenStream {
-    //style::macro_fn(input.into()).into()
-    Default::default()
-}
-
-#[proc_macro]
-#[proc_macro_error]
-pub fn global_style(input: TokenStream) -> TokenStream {
-    //global_style::macro_fn(input.into()).into()
-    Default::default()
-}
-
-#[proc_macro]
-#[proc_macro_error]
-pub fn css(input: TokenStream) -> TokenStream {
-    //css::macro_fn(input.into()).into()
-    Default::default()
-}
-
-#[proc_macro]
-#[proc_macro_error]
-pub fn use_style(input: TokenStream) -> TokenStream {
-    //use_style::macro_fn(input.into()).into()
-    Default::default()
-}
-
-#[proc_macro_attribute]
-pub fn styled_component(attr: TokenStream, item: TokenStream) -> TokenStream {
-    //styled_component::macro_fn(attr, item)
-    Default::default()
-}
+// Error.
+use stylist_core::Error;
 
 #[proc_macro_attribute]
 pub fn styled_component_impl(attr: TokenStream, item: TokenStream) -> TokenStream {
-    //styled_component_impl::macro_fn(attr, item)
     Default::default()
 }
